3cb7124d5bbc9833bb75160fa35917d2dc91e962
Kun <quinnHugh@hotmail.com>
2019-08-15 09:31:30 +0800

    update modified

--- hexo/docker-entrypoint.sh
+++ hexo/docker-entrypoint.sh
@@ -5,7 +5,7 @@ echo "$@" | awk -F ' ' '{print $1}' | xargs git config --global user.name
 echo "$@" | awk -F ' ' '{print $2}' | xargs git config --global user.email
 
 if [ "$3" = 's' ] || [ "$3" = 'server' ]; then
-    set -- /usr/bin/hexo s -p 80
+    set -- /usr/bin/hexo s -p 8080
 fi
 
 if [ "$3" = 'd' ] || [ "$3" = 'deploy' ]; then

59924d9deaae70bb01d9ea586091fddcb19a6233
Kun <quinnHugh@hotmail.com>
2019-08-22 14:26:22 +0800

    Update docker-entrypoint.sh

--- hexo/docker-entrypoint.sh
+++ hexo/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-echo "$@" | awk -F ' ' '{print $1}' | xargs git config --global user.name
+# echo "$@" | awk -F ' ' '{print $1}' | xargs git config --global user.name
 
-echo "$@" | awk -F ' ' '{print $2}' | xargs git config --global user.email
+# echo "$@" | awk -F ' ' '{print $2}' | xargs git config --global user.email
 
 if [ "$3" = 's' ] || [ "$3" = 'server' ]; then
     set -- /usr/bin/hexo s -p 8080
